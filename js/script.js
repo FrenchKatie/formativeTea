41f2a83e1799d772c6176a3298cd3f0c6fd43246
Katie elizabeth French <845855192@bun2-206-16.local>
2018-03-23 11:51:49 +1300

    fifth commit

--- js/script.js
+++ js/script.js
@@ -2,35 +2,13 @@ $(document).ready(function(){
   $(".teasUl").click(function(){
     $(".nestedUl").slideToggle();
   })
-  // $(".bla").click(function(){
-  //   $(".mainUl").slideToggle();
-  //   $("body").toggleClass('stopScroll');
-  // })
+
 
   $(".bla").click(function(){
     $(".mainUl").slideToggle();
-    $("html, body").toggleClass('stopScroll');
-  });
-})
+    $("body").toggleClass('stopScroll');
+    $("header").toggleClass('scroll');
+    $(".bla").toggleClass('fixed');
+  })
 
-
-// $('.bla').click(function() {
-//     var self = $(this);
-//      $(".mainUl").slideToggle();
-//     self.toggleClass('open');
-//     $('.mainUl').toggleClass('open');
-//     $("body").toggleClass('stopScroll');
-//
-//     if (self.hasClass('open')) {
-//         $('html, body')
-//             // Needed to remove previously bound handlers
-//             .off('touchstart touchmove')
-//             .on('touchstart touchmove', function (e) {
-//                 e.preventDefault();
-//             });
-//     } else {
-//         $('html, body')
-//             .off('touchstart touchmove')
-//             .on('touchstart touchmove', function (e) {});
-//     }
-// });
+});
