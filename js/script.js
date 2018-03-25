a5ffc29012e18e3a208493fe8a4787215c60deba
Katie elizabeth French <845855192@bun2-206-16.local>
2018-03-26 11:54:23 +1300

    Seventh Commit

--- js/script.js
+++ js/script.js
@@ -4,11 +4,39 @@ $(document).ready(function(){
   })
 
 
-  $(".bla").click(function(){
+  $(".toggleMobNav").click(function(){
     $(".mainUl").slideToggle();
     $("body").toggleClass('stopScroll');
     $("header").toggleClass('scroll');
-    $(".bla").toggleClass('fixed');
+    $(".toggleMobNav").toggleClass('fixed');
   })
+
+  $('#nav-icon3').click(function(){
+    $(this).toggleClass('open');
+  });
+
+  // $('.teasUl').on("click", function(){
+  //   var $arrows = $(this).find("i");
+  //   $('.open-close-toggle').toggle(function(){
+  //     $arrows.toggle();
+  //   })
+  // });
+  // 
+  // $( ".mainUl" ).toggle({
+  //     heightStyle: "content",
+  //     active:false,
+  //     collapsible: true,
+  //     header:"div.accordianheader"
+  //   });
+
+
+
+
+
+
+    // $('.toggle-section').slideToggle(function(){
+    //     $arrows.toggle();
+    // });
+
 
 });
